33cc25e5d84a8d0e8606529483aa1229c61f1349
nonlinear <git@non-linear.net>
2023-11-23 23:05:37 +0100

    modified struct

--- http-server-lp-author/struct/src/main.rs
+++ http-server-lp-author/struct/src/main.rs
@@ -1,9 +1,11 @@
 #[derive(Debug)]
 enum Lang {
-  English,
-  Spanish,
-  Chinese,
-  Texan,
+    English,
+    Spanish,
+    Chinese,
+    Texan,
+    Catalan,
+    Dogolon,
 }
 
 struct Greeting {
@@ -12,18 +14,40 @@ struct Greeting {
 }
 
 fn main() {
-  let mut v :Vec<Greeting> = Vec::new();
+    let mut v: Vec<Greeting> = Vec::new();
 
-  let g : Greeting = Greeting { lang: Lang::English, message: String::from("Hello WasmEdge!") };
-  v.push(g);
-  let g : Greeting = Greeting { lang: Lang::Spanish, message: String::from("Hola WasmEdge!") };
-  v.push(g);
-  let g : Greeting = Greeting { lang: Lang::Texan, message: String::from("Howdy WasmEdge!") };
-  v.push(g);
-  let g : Greeting = Greeting { lang: Lang::Chinese, message: String::from("WasmEdge 你好!") };
-  v.push(g);
+    let g: Greeting = Greeting {
+        lang: Lang::English,
+        message: String::from("Hello WasmEdge!"),
+    };
+    v.push(g);
+    let g: Greeting = Greeting {
+        lang: Lang::Spanish,
+        message: String::from("Hola WasmEdge!"),
+    };
+    v.push(g);
+    let g: Greeting = Greeting {
+        lang: Lang::Texan,
+        message: String::from("Howdy WasmEdge!"),
+    };
+    v.push(g);
+    let g: Greeting = Greeting {
+        lang: Lang::Chinese,
+        message: String::from("WasmEdge 你好!"),
+    };
+    v.push(g);
+    let g: Greeting = Greeting {
+        lang: Lang::Catalan,
+        message: String::from("Bon dia, WasmEdge!"),
+    };
+    v.push(g);
+    let g: Greeting = Greeting {
+        lang: Lang::Dogolon,
+        message: String::from("Wuff, wuff, WasmEdge!"),
+    };
+    v.push(g);
 
-  for e in v {
-    println!("{:?} {}", e.lang, e.message);
-  }
+    for e in v {
+        println!("{:?} {}", e.lang, e.message);
+    }
 }
